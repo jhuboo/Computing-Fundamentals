e56f81e306f0b76dc5428ec38794205e3d80cebd
Anvesh Jhuboo <rgjhuboo@gmail.com>
2020-07-14 13:07:42 -0400

    cla

---
+++ cla_ProgrammingFundamentals/2_chapter/squareroot.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+	float value, squareroot;
+	
+	printf("Give me a number and I will find its square root:\n");
+	scanf("%f", &value);
+	if (value >= 0.0) {
+		squareroot = sqrt(value);
+		printf("You have given: %f\n", value);
+		printf("The square root is: %f\n", squareroot);
+	}
+	return 0;
+}
